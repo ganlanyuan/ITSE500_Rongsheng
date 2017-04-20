bc22e54bc09abf592be2846d206d1702cd58664f
William Lin 22 <ganlanyuan@gmail.com>
2017-04-20 15:03:18 -0400

    initial commit

---
+++ src/edu/oit/lesson3/JavaInput.java
@@ -0,0 +1,19 @@
+package edu.olivet.itse500;
+
+import java.util.Scanner;
+
+public class JavaInput {
+    public static void main(String[] args) {
+        int number1, number2, sum;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first multiple:");
+        number1 = input.nextInt();
+        
+        System.out.println("Enter second multiple:");
+        number2 = input.nextInt();
+        
+        sum = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + sum);
+    }
+}
