3c93ed1f69788b74b2d4015d01b517d7e63e92d5
William Lin 22 <ganlanyuan@gmail.com>
2017-04-29 19:10:56 -0400

    add lesson5

---
+++ src/edu/oit/lesson5/Swimable.java
@@ -0,0 +1,5 @@
+package edu.oit.lesson5;
+
+public interface Swimable {
+    void swim();
+}
---
+++ src/edu/oit/lesson5/Whale.java
@@ -0,0 +1,10 @@
+package edu.oit.lesson5;
+
+public class Whale implements Swimable {
+    
+    @Override
+    public void swim() {
+        System.out.println("swim under water");
+    }
+    
+}
